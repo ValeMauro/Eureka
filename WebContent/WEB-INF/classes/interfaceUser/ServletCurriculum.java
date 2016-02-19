51bb3a2a3da80d2419367f9f77f72e7544de23f8
lorella <alenia@alenia-ultrabook>
2016-02-19 14:55:35 +0100

    Unione servletFinale e servletCurriculum in servletCurriculum

--- WebContent/WEB-INF/classes/interfaceUser/ServletCurriculum.java
+++ WebContent/WEB-INF/classes/interfaceUser/ServletCurriculum.java
@@ -15,9 +15,41 @@ import tokenization.DocParser;
 import tokenization.TagParser;
 
 public class ServletCurriculum extends HttpServlet {
+	private List<String> l= new LinkedList<String>();
+	private String cv="";
+	
+//	private class Curriculum{
+//		private List<String> listaAttributi= new LinkedList<String>();
+//		private String cv="";
+//		
+//		public Curriculum(){}
+//		
+//		public List<String> getListaAttributi(){
+//			return listaAttributi;
+//		}
+//		
+//		public String getCv(){
+//			return cv;
+//		}
+//		
+//		public void setListaAttributi(List<String> list){
+//			listaAttributi= list;
+//		}
+//		
+//		public void setCv(String s){
+//			cv=s;
+//		}
+//		
+//		
+//		
+//	}
+//	
+	
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String Curriculum = ""+req.getParameter("curriculum");
+		this.cv = ""+req.getParameter("curriculum").toString();
+		
 		char a34 = 34;//serve per i doppi apici 34 in ascii ""
 		int numero=0;
 		//	DocParser CV =new DocParser();
@@ -26,7 +58,7 @@ public class ServletCurriculum extends HttpServlet {
 		TagParser tagParser = new TagParser();
 		List<String> listaAttributi = new LinkedList<String>();
 	try {
-			listaAttributi.addAll(tagParser.parseTags(Curriculum));
+			listaAttributi.addAll(tagParser.parseTags(this.cv));
 		} catch (UnirestException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -115,7 +147,7 @@ public class ServletCurriculum extends HttpServlet {
 				//form o box dove andranno tutti gli attributi
 				
 				//pulsante di fine
-		 "<form name='box_Attributi' id='box_Attributi' action="+a34+"servletFinale"+a34+" method="+a34+"get"+a34+">"		
+		 "<form name='box_Attributi' id='box_Attributi' action="+a34+"servletCurriculum"+a34+" method="+a34+"post"+a34+">"		
 		
 		 +"<DIV ALIGN="+a34+"center"+a34+">"
 		
@@ -146,8 +178,8 @@ public class ServletCurriculum extends HttpServlet {
 		
 		//chiusura box per aggiungere gli attributi
 		resp.getWriter().println(""
-				//testo del curriculum nascosto
-		 		+"<textarea onSubmit='return false' hidden =true name="+a34+"curriculum"+a34+" rows="+a34+"20"+a34+" cols="+a34+"20"+a34+" >"+Curriculum+"</textarea>"
+	//			//testo del curriculum nascosto
+	//	 		+"<textarea onSubmit='return false' hidden =true name="+a34+"curriculum"+a34+" rows="+a34+"20"+a34+" cols="+a34+"20"+a34+" >"+Curriculum+"</textarea>"
 		
 		//text nascosta che contiene il numero di tutti gli attributi con nome Attributi+numero da 0 in poi che l'utente vuole per essere ricercato		
 				+ "<input type='text' hidden='1' name='NumeroDiOggettiInseritiDallUtente' value="+a34+numero+a34+">"
@@ -166,4 +198,87 @@ public class ServletCurriculum extends HttpServlet {
 		//chiusura html
 		 "</body></html>");
 	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		char a34 = 34;//serve per i doppi apici 34 in ascii ""
+		
+		
+		
+	//curriculum
+	//	String CurriculumPPP = ""+req.getParameter("curriculum");
+	//	String Curriculum=CurriculumPPP.toString();
+		
+		
+	//creo la lista di attributi
+		List<String> ListaAttributi= new LinkedList<String>();
+		String stringaNumeroAttributi= req.getParameter("NumeroDiOggettiInseritiDallUtente");
+		//recupero ogni attributoDellaPersna: ogni parametro si chiama 'Attributo"+numero+"'
+		int numeroAttributi = Integer.parseInt(stringaNumeroAttributi);
+		String Attributo="";
+		String NonSonoUnTag="NonSonoUnTag";
+		for (int i = 0; i < numeroAttributi; i++) {
+			Attributo =""+ req.getParameter("AttributoTxt"+i);
+			if (!(Attributo.equals(NonSonoUnTag))) {
+				ListaAttributi.add(Attributo);
+				}
+		}
+		
+	 	
+	//salvataggio curriculum	
+		TagParser tagParser = new TagParser();
+		/* 
+		try {
+			tagParser.parseTags(Curriculum);
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+		 
+		 try {
+			tagParser.saveCV(this.cv,ListaAttributi);
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+			
+		//apertura html
+			resp.getWriter().println("<html>"+
+					"<head>"+
+					"<title>TestForm</title>"+
+					"</head>"+
+					"<body>"+
+
+					//link per tornare alla home page
+					"<DIV ALIGN="+a34+"right"+a34+">"+
+						"<font face="+a34+"Times New Roman"+a34+" size="+a34+"3"+a34+"><dfn>"+
+						"<a href="+a34+"index.html"+a34+">ritorna alla home</a> <br/>"+
+					"</dfn></font></div>"+
+
+					//messaggio iniziale blu
+					"<div style="+a34+"position: absolute; width: 500px; height: 200px; left: 50%; top: 50%; margin-left: -250px; margin-top: -100px; text-align: center;"+a34+">"+
+					"<font face="+a34+"Times New Roman"+a34+" size="+a34+"5"+a34+" color="+a34+"blue"+a34+">"+
+					"Congratulazioni hai terminato!<br/>"+
+					"</font>"+
+
+
+					//istruzioni in rosso
+					"<font face="+a34+"Times New Roman"+a34+" size="+a34+"3"+a34+" color="+a34+"red"+a34+">"+
+					"per tonare alla home clicca qua sotto o in alto a destra <br/>"+
+					"</font>"+
+					
+					//altro link alla homepage
+					"<font face="+a34+"Times New Roman"+a34+" size="+a34+"3"+a34+"><dfn>"+
+					"<a href="+a34+"index.html"+a34+">ritorna alla home</a> <br/>"+
+					"</dfn></font>"+
+					"</div>"+
+					
+					//chiusura html
+					"</body></html>");
+	
+
+		
+	}
+
 }
