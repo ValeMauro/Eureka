bce8ea19cfe44d1baccd5f4c3d7527c501de050f
lorella <alenia@alenia-ultrabook>
2016-04-02 15:54:13 +0200

    PageParser carica le pagine da locale (parsing da finire)

--- WebContent/WEB-INF/classes/parsing/PageParserTest.java
+++ WebContent/WEB-INF/classes/parsing/PageParserTest.java
@@ -1,44 +1,13 @@
 package parsing;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
 
 public class PageParserTest {
 
 	public static void main(String[] args) throws IOException {
-
-		HttpGet request = new HttpGet("http://www.oracle.com/");
-		HttpClient httpClient = HttpClientBuilder.create().build();
-		HttpResponse response = httpClient.execute(request);			
-		HttpEntity entity = response.getEntity();
-		String responseString = EntityUtils.toString(entity, "UTF-8");
-		System.out.println(responseString);
 		
-//		URL oracle;
-//		oracle = new URL("http://www.oracle.com/");
-//		try {
-//			BufferedReader in = new BufferedReader(
-//					new InputStreamReader(oracle.openStream()));
-//
-//			String inputLine;
-//			while ((inputLine = in.readLine()) != null)
-//				System.out.println(inputLine);
-//			in.close();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		PageParser pp = new PageParser();
+		pp.ParsePages();
 	}
 
 }
--- src/parsing/PageParser.java
+++ src/parsing/PageParser.java
@@ -1,62 +1,59 @@
 package parsing;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
+import java.util.LinkedList;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import init.Result;
 
 public class PageParser {
-	
-	public void ParsePage(LinkedList<Result> lista){
-		
-		for (Result res : lista) {
-			try {
-				//prende la pagina html dall'url dato
-				HttpGet request = new HttpGet(res.getUrl());
-				HttpClient httpClient = HttpClientBuilder.create().build();
-				HttpResponse response = httpClient.execute(request);			
-				HttpEntity entity = response.getEntity();
-				//trasformo la pagina html in stringa
-				String responseString = EntityUtils.toString(entity, "UTF-8");
+	private final String PATH = "websites";
+	private LinkedList<Result> resultSet;
 
-				//salva la pagina in locale
-				savePageToLocal(responseString);
-				//pulisce l'html della pagina e lo salva in res
-				String cleanText = cleanHtmlText(responseString);
-				res.setText(cleanText);
-			} catch (ClientProtocolException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (UnsupportedOperationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
+	public PageParser(){
+		resultSet = new LinkedList<Result>();
+	}
+
+	//carica i file della cartella websites e li parsa usando la lib JSoup
+	public LinkedList<Result> ParsePages(){
+
+		File input = new File(PATH);
+		File[] st = input.listFiles();
+		for (int i = 0; i < st.length; i++) {
+			if(st[i].isDirectory()){
+				File file = new File(PATH, st[i].getName());
+				File[] fileList = file.listFiles();
+				for(int j = 0; j < fileList.length; j++){
+					if(fileList[i].isFile()){
+						//System.out.println(fileList[i].getName());
+						resultSet.add(parse(st[i]));
+					}
+				}
+			}
 		}
-		
+		return resultSet;
 	}
 
-	//metodo per salvare la pagina html in locale
-	private void savePageToLocal(String responseString) {
-		// TODO Auto-generated method stub
-		
-	}
+	//dato un file, lo parsa in un oggetto Result
+	private Result parse(File file) {
+		// TODO sistemare bene in modo che compili il Result
+		Document doc = new Document("");
 
-	//metodo per pulire la pagina html
-	private String cleanHtmlText(String string) {
-		// TODO Auto-generated method stub
+		try {
+			doc = Jsoup.parse(file, "UTF-8", "");
+		} catch (IOException e) {
+			System.out.println("Errore di parsing JSoup");
+		}
+		Elements ids = doc.select("div[id^=desk] p");
+		for (Element id : ids){
+			System.out.println("\n"+id.text());
+		}
+
 		return null;
 	}
-
 }
