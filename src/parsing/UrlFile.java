ac49e83d77f76a14cd5027d9ad50a570fe9b613b
valeria <vale.mauro2@gmail.com>
2016-04-04 21:14:20 +0200

    Add file urlBingProva, update UrlBing, update class urlFile

--- src/parsing/UrlFile.java
+++ src/parsing/UrlFile.java
@@ -44,20 +44,56 @@ public class UrlFile {
 			BufferedReader br = new BufferedReader(new FileReader("UrlBing"));
 			String s= br.readLine();
 			while(s!=null){
-				String init=s.substring(0,5);
-				if(init.equals("mkdir")) {
-					String temp=s.substring(6, s.length());
-					name=temp.replace("_", " ");
-				}
-				else{
-					if(init.equals("wget ")){ 
-						Result r= new Result();
-						r.setSource(name);
-						String temp=s.substring(5, s.length());
-						r.setUrl(temp);
-						res.add(r);
+				if(s.length()>=5){
+					String init=s.substring(0,5);
+					if(init.equals("mkdir")) {
+						String temp=s.substring(6, s.length());
+						name=temp.replace("_", " ");
+					}
+					else{
+						if(init.equals("wget ")){ 
+							Result r= new Result();
+							r.setSource(name);
+							String temp=s.substring(5, s.length());
+							r.setUrl(temp);
+							res.add(r);
+						}
 					}
 				}
+				
+				s= br.readLine();
+			}
+			br.close();
+		} catch (IOException e) {
+			System.out.println("Errore apertura file UrlBing");
+		}
+		return res;	
+	}
+	
+	public static LinkedList<Result> readFileProva() throws FileNotFoundException {
+		LinkedList<Result> res = new LinkedList<Result>();
+		String name="";
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("UrlBingProva"));
+			String s= br.readLine();
+			while(s!=null){
+				if(s.length()>=5){
+					String init=s.substring(0,5);
+					if(init.equals("mkdir")) {
+						String temp=s.substring(6, s.length());
+						name=temp.replace("_", " ");
+					}
+					else{
+						if(init.equals("wget ")){ 
+							Result r= new Result();
+							r.setSource(name);
+							String temp=s.substring(5, s.length());
+							r.setUrl(temp);
+							res.add(r);
+						}
+					}
+				}
+				
 				s= br.readLine();
 			}
 			br.close();
