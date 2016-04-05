ddace713b8fd81dee9e21a0f33d73b372afde649
lorella <alenia@alenia-ultrabook>
2016-04-05 15:31:16 +0200

    PageParsing now also saves pages to local

--- src/parsing/PageParserTest.java
+++ src/parsing/PageParserTest.java
@@ -23,7 +23,8 @@ public class PageParserTest {
 	public static void main(String[] args) throws IOException {
 		LinkedList<Result> lista = new LinkedList<Result>();
 		Result res = new Result();
-		res.setUrl("http://jsoup.org/cookbook/introduction/parsing-a-document");
+		res.setUrl("https://sites.google.com/a/uniroma1.it/achillepaolone/");
+		res.setSource("Paolone Achille");
 		lista.add(res);
 		
 		PageParser pp = new PageParser();
