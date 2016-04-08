f6d28f50e9fb4e5c59853412a9edfc43296d257f
valeria <vale.mauro2@gmail.com>
2016-04-08 15:12:31 +0200

    Class Test

--- WebContent/WEB-INF/classes/lucene/Repository.java
+++ WebContent/WEB-INF/classes/lucene/Repository.java
@@ -88,7 +88,7 @@ public class Repository {
 			doc.add(new TextField(SUBTITLE, res.getSubtitle(), Field.Store.YES));
 			doc.add(new TextField(TEXT, res.getText(), Field.Store.YES));
 			doc.add(new TextField(URL, res.getUrl(), Field.Store.YES));
-			doc.add(new StringField(DATE, res.getDate(), Field.Store.YES));
+			doc.add(new TextField(DATE, res.getDate(), Field.Store.YES));
 			doc.add(new DoubleField(RANK, res.getRank(), Field.Store.YES));
 
 			// aggiungo doc al file
--- src/init/Test.java
+++ src/init/Test.java
@@ -14,26 +14,35 @@ public class Test {
 //		for (Similar similar : sims) {
 //			System.out.println(similar.getName()+" "+similar.getGrade());
 //		}
-		
-		LinkedList<Result> list= new LinkedList<Result>();
-		Result r1= new Result();
-		r1.setDate("02/11/1800");
-		Result r2= new Result();
-		Result r3= new Result();
-		r3.setDate("ciao");
-		Result r4= new Result();
-		r4.setDate("05/12/1900");
-		Result r5= new Result();
-		r5.setDate("falso");
-		list.add(r1);
-		list.add(r2);
-		list.add(r3);
-		list.add(r4);
-		list.add(r5);
-		LinkedList<Result> res= Result.orderByDate(list);
-		for (Result result : res) {
-			System.out.println(result.getDate());
-		}
+//		order();
+		LinkedList<Result> res=rep.searchLucene("Achille");
+		System.out.println(res.size());
 	}
+	
+	
+		private static void order() {
+			LinkedList<Result> list= new LinkedList<Result>();
+			Result r1= new Result();
+			r1.setDate("02/11/1800");
+			Result r2= new Result();
+			Result r3= new Result();
+			r3.setDate("ciao");
+			Result r4= new Result();
+			r4.setDate("05/12/1900");
+			Result r5= new Result();
+			r5.setDate("falso");
+			list.add(r1);
+			list.add(r2);
+			list.add(r3);
+			list.add(r4);
+			list.add(r5);
+			LinkedList<Result> res= Result.orderByDate(list);
+			for (Result result : res) {
+				System.out.println(result.getDate());
+			}
+		}
+		
+	
+		
 	
 }
