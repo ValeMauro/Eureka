52614b9b24f207d9ea49cd241bd649e3d3e99a61
lorella <alenia@alenia-ultrabook>
2016-04-08 18:53:54 +0200

    Cleaned PageParser

--- src/parsing/PageParser.java
+++ src/parsing/PageParser.java
@@ -80,10 +80,6 @@ public class PageParser {
 				//parsa la pagina html in Result res
 				parseHTML(res, responseString);
 
-				//print di check
-				//				System.out.println("Title: " + res.getTitle());
-				//				System.out.println("SubTitles: " + res.getSubtitle());
-				//				System.out.println("Last-modified: " + res.getDate());
 
 			} catch (ClientProtocolException e) {
 				e.getMessage();
@@ -111,15 +107,6 @@ public class PageParser {
 		else res.setTitle("");
 
 		//text di res viene settato col contenuto di tutti i tag <p>
-		//TODO fare le prove con i div?
-		//				Elements text_el = doc.select("p");
-		//				String text = "";
-		//				if(text_el!=null){
-		//					for (Element e : text_el){
-		//						text = text+e.text()+ " ";
-		//					}
-		//				}
-		//				res.setText(text);
 		Element text_el = doc.select("body").first();
 		if(text_el!=null){
 			res.setText(text_el.text());
@@ -138,19 +125,12 @@ public class PageParser {
 		String folder_name = res.getSource().replace(" ", "_");
 		File folder = new File(ROOT, folder_name);
 		if (!folder.exists()){
-//			try{
-		        folder.mkdir();
-//		    } 
-//		    catch(SecurityException e){
-//		        e.getMessage();
-//		    }    
+	        folder.mkdir();   
 		}
 		File[] folder_list = folder.listFiles();
 		int counter = folder_list.length+1;
 		String file_name = "";
 		String url = res.getUrl();
-//		String extention = url.substring(url.lastIndexOf('.'));
-//		file_name = folder_name + Integer.toString(counter) + extention;
 		if(!url.substring(url.lastIndexOf('.')).equals(".pdf")){
 			file_name = folder_name + Integer.toString(counter) + ".html";
 		} else file_name = folder_name + Integer.toString(counter) + ".pdf";
